d501fcd124a042b4eab622553b7ca741b8c5fe53
kamilovkz <001kamilov@gmail.com>
2022-05-25 12:02:00 +0600

    Add timer and Video

--- exercise79/ContentView.swift
+++ exercise79/ContentView.swift
@@ -6,29 +6,77 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    let videoNames = ["video1", "video2", "video3"]
+//    let someNames = ["First", "Second", "Third"]
+    let index: Int
+    
     var body: some View {
-        ZStack {
-            ExtractedView()
-                .padding()
+        VStack {
+            TabView {
+                welcomeScreen(someNames: videoNames[index])
+                secondSreen()
+                thirdSrcreen()
+            }.tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(
+                    PageIndexViewStyle(backgroundDisplayMode: .always))
         }
+    }
+}
+
+struct welcomeScreen: View {
+    let someNames: String
+    let interval: TimeInterval = 30
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                if let url = Bundle.main.url(forResource: someNames, withExtension: "mp4") {
+                    VideoPlayer(player: AVPlayer(url: url))
+                        .frame(height: geometry.size.height * 0.45)
+                        .padding(.bottom)
+                        .tabItem{ Text("WELCOME") }
+                } else {
+                    Text("Couldn't find \(someNames).mp4")
+                        .foregroundColor(.yellow)
+                }
+                VStack {
+                    Text(someNames)
+                        .font(.largeTitle)
+                    HStack {
+                        Image(systemName: "1.circle")
+                        Image(systemName: "2.circle")
+                        Image(systemName: "3.circle")
+                        Image(systemName: "4.circle")
+                    }.font(.title)
+                    Text(Date().addingTimeInterval(interval), style: .timer)
+                        .font(.system(size: 90))
+                }
+            }
+        }
+    }
+}
+
+struct secondSreen: View {
+    var body: some View {
+        Text("Second Video")
+            .tabItem{ Text("Second Video") }
+    }
+}
+
+struct thirdSrcreen: View {
+    var body: some View {
+        Text("Third video")
+            .tabItem{ Text("Third Video") }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
-}
-
-struct ExtractedView: View {
-    var body: some View {
-        HStack {
-            Image(systemName: "1.circle")
-            Image(systemName: "2.circle")
-            Image(systemName: "3.circle")
-            Image(systemName: "4.circle")
-        }.font(.largeTitle)
+        ContentView(index: 0)
+            .previewLayout(.sizeThatFits)
+            .previewInterfaceOrientation(.portrait)
+            .preferredColorScheme(.dark)
     }
 }
